2ef7b262309ce3d3b92c39a22e5ccb3c49f5eb29
Chen-Dixi <cdxcqu2014@163.com>
2019-03-27 21:23:30 +0800

    fix mnist transform bug

--- data/datasetsFactory.py
+++ data/datasetsFactory.py
@@ -4,12 +4,11 @@ from torch.utils.data import DataLoader
 import os
 
 def create_data_loader(dataset, root, batch_size):
-    dataset_mean = (0.5, 0.5, 0.5)
-    dataset_std = (0.5, 0.5, 0.5)
+    
     if dataset.lower() == "mnist":
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(  mean=dataset_mean, std=dataset_std)
+            transforms.Normalize(mean=(0.1307,), std=(0.3081,))
             ])
 
         #Prepa Data  ,Train, Test分开
@@ -22,11 +21,40 @@ def create_data_loader(dataset, root, batch_size):
         
         
     elif dataset.lower() == "mnist_m":
+        dataset_mean = (0.5, 0.5, 0.5)
+        dataset_std = (0.5, 0.5, 0.5)
         transform = transforms.Compose([
                 transforms.Resize([28,28]),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=dataset_mean,std=dataset_std)
             ])
+
+        #mnist_m
+        trainSets = datasets.ImageFolder(root=os.path.join(root,'train'),transform=transform)
+        trainDataloader = DataLoader(dataset=trainSets,batch_size=batch_size,shuffle=True)
+
+        testSets = datasets.ImageFolder(root=os.path.join(root,'test'),transform=transform)
+        
+
+        testDataloader = DataLoader(dataset=testSets,batch_size=batch_size,shuffle=True)
+    return trainDataloader, testDataloader
+
+def create_data_loader_with_transform(dataset, root, batch_size, transform=None):
+    
+    if dataset.lower() == "mnist":
+        
+
+        #Prepa Data  ,Train, Test分开
+        #mnist
+        trainSets = datasets.MNIST(root,train=True,download=False,transform=transform)
+        trainDataloader = DataLoader(dataset=trainSets,batch_size=batch_size,shuffle=True)
+
+        testSets = datasets.MNIST(root,train=False,download=False,transform=transform)
+        testDataloader = DataLoader(dataset=testSets,batch_size=batch_size,shuffle=True)
+        
+        
+    elif dataset.lower() == "mnist_m":
+        
 
         #mnist_m
         trainSets = datasets.ImageFolder(root=os.path.join(root,'train'),transform=transform)
