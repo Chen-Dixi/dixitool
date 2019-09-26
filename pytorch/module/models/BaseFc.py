ed565d33fc12f557d9229094559efcf81e10bd64
Chen-Dixi <cdxcqu2014@163.com>
2019-09-27 01:28:53 +0800

    Module FC init params method

--- pytorch/module/models/BaseFc.py
+++ pytorch/module/models/BaseFc.py
@@ -10,4 +10,14 @@ class BaseFeatureExtractor(nn.Module):
     def output_num(self):
         pass
 
-    
+    def _init_params(self):
+        for m in self.modules:
+            if isinstance(m ,nn.Conv2d)
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinear='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias,0)
+            if isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
