635e10c577ae395ecccc589fca8ba8d172b2205f
Chen-Dixi <cdxcqu2014@163.com>
2019-10-23 21:47:58 +0800

    提督反转处理一个问题

--- pytorch/module/gradient_reverse.py
+++ pytorch/module/gradient_reverse.py
@@ -3,17 +3,18 @@ from torch.autograd import Function
 import torch
 import torch.nn as nn
 
+#这个代码有很大的gpu tensor 和cpu tensor不能一起计算的问题
 class GradReverse(Function):
 
     #pytorch 1.0之后都只能在
     @staticmethod
     def forward(ctx, x, lambd):
-        ctx.save_for_backward(lambd)
+        ctx.lambd = lambd
         return x.view_as(x)
 
     @staticmethod
     def backward(ctx, grad_output):
-        lambd = ctx.saved_tensors[0]
+        lambd = ctx.lambd
         return grad_output.neg()*lambd, None
 
 def grad_reverse(x, lambd):
@@ -21,7 +22,7 @@ def grad_reverse(x, lambd):
 
 # Use grl as a module
 def identity_scheduler(global_step):
-    return torch.ones(1)
+    return torch.ones(1).item()
 
 #lambda 在这一层里面计算
 class GradReverseLayer(nn.Module):
@@ -32,11 +33,11 @@ class GradReverseLayer(nn.Module):
         self.scheduler = scheduler
         self.register_buffer('global_step', torch.zeros(1))
 
-
+    #注意这里的
     def forward(self, x , lambd=None):
         #1个batch就加一个1
         if lambd is None:
-            lambd = self.scheduler(self.global_step)#lambd是一个torch tensor
+            lambd = self.scheduler(self.global_step.item())#lambd是一个torch tensor
         
         if self.training:
             self.global_step += 1.0
