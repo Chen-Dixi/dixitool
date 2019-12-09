dc0e84b1dd720f4b6a58b7ad680ba099dfa6669c
Chen-Dixi <cdxcqu2014@163.com>
2019-12-09 09:02:52 +0800

    update

--- pytorch/module/models/lenet.py
+++ pytorch/module/models/lenet.py
@@ -18,5 +18,5 @@ class LeNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return x
 
