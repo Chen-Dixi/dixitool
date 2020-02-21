8ac67037aef05ddbd01b4c5d8b89905b1c1f3e87
Chen-Dixi <cdxcqu2014@163.com>
2020-02-21 09:18:55 +0800

    常用实验模版框架

---
+++ researchTool/Templetes/RegularExperiments/_ProjectName_/functions.py
@@ -0,0 +1,32 @@
+import torch
+import random
+import numpy as np 
+
+def print_config(args):
+    print("==========================================")
+    print("==========       CONFIG      =============")
+    print("==========================================")
+    for arg, content in args.__dict__.items():
+        print("{}:{}".format(arg, content))
+    print("\n")
+
+ 
+def post_config(opt):
+    opt.device = torch.device('cuda:%s'%(opt.cuda) if torch.cuda.is_available() else "cpu")
+    if opt.seed is None:
+        opt.seed = random.randint(1, 10000)
+    
+    random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed_all(opt.seed)
+    np.random.seed(opt.seed)
+    if torch.cuda.is_available() and opt.not_cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    
+    #输出文件
+    opt.dir2save = generate_dir2save(opt)
+    return opt
+
+def generate_dir2save(opt):
+    dir2save = "TrainedModel/%s/%d" % (opt.net, opt.build)
+    return dir2save
---
+++ researchTool/Templetes/RegularExperiments/main_train.py
@@ -0,0 +1,35 @@
+import _ProjectName_.functions as functions
+from config import get_train_arguments
+from training import *
+import random
+#都可以用的就放里面，在另外定制的加外面
+parser = get_train_arguments()
+#get_train_arguments里面就放必要的arguments
+#接下来 parser里面可以加一些别的东西
+#parser.add_argument('--checkpoint-file-name', default='step2checkpoint.pth.tar', type=str, metavar='checkpoint-file-name',
+#  help='name for checkpoint files') #只给文件名，一般后面的处理都会在path前面加一个checkpoints文件夹
+
+#设置，所以叫option
+opt = parser.parse_args()
+
+opt=functions.post_config(opt) #给args加一些 argparse加不了的东西
+functions.print_config(opt) #打印 parser.parse_args()里面的东西
+
+#先生成要保存的地址
+if (os.path.exists(opt.dir2save)):
+    print('trained model already exist')
+else:
+    try:
+        os.makedirs(opt.dir2save)
+    except OSError:
+        pass
+
+
+#找 model
+
+#记载数据集
+
+#accMetric
+
+
+
