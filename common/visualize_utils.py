1ca282d0e1e532569e7f24bd07331f65f0ab5b88
Chen-Dixi <cdxcqu2014@163.com>
2022-02-22 17:59:16 +0800

    udpate visualization function

--- common/visualize_utils.py
+++ common/visualize_utils.py
@@ -4,7 +4,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
-from typing import Tuple
+from typing import List
 
 def plot_2d(cartesian_CG, out_CG, filename:str, cartesian_color = 'r', out_color = 'b'):
     """
@@ -26,7 +26,7 @@ def plot_2d(cartesian_CG, out_CG, filename:str, cartesian_color = 'r', out_color
     # output image
     plt.savefig(filename)
 
-def plot_3d_wireframe(data: Tuple, title: Tuple[str, ...], color: Tuple[str, ...], savefile:str):
+def plot_3d_wireframe(data: List, title: List[str], color: List[str], savefile:str):
     """
     3D Plot, 用matlibplot画3D wirefram图
     Args:
@@ -34,20 +34,22 @@ def plot_3d_wireframe(data: Tuple, title: Tuple[str, ...], color: Tuple[str, ...
     """
     sz = len(title)
     assert len(data) == len(title) == len(color)
-    aspect = 1 / sz
-    fig = plt.figure(figsize=plt.figaspect(aspect), facecolor=(1., 1., 1.))
-    for i in range(1,sz+1):
-
-        wireframe = fig.add_subplot(1,sz,i, projection='3d')
-        x1,y1,v1 = data[i-1]
-        wireframe.plot_wireframe(x1, y1, v1, colors=color[i-1])
+    row = (sz // 2) + (sz % 2)
+    col = 2
+    fig, axs = plt.subplots(row, col, subplot_kw={'projection':'3d'},figsize=[col*6.4, row*4.8])
+    for index in range(sz):
+        i = index // 2
+        j = index % 2
+        x1,y1,v1 = data[index]
+        axs[i,j].plot_wireframe(x1, y1, v1, colors=color[index])
         # title
-        wireframe.set_title(title[i-1])
+        axs[i,j].set_title(title[index])
         # label
-        wireframe.set_xlabel('x')
-        wireframe.set_ylabel('y')
-        wireframe.set_zlabel('value')
-
+        axs[i,j].set_xlabel('x')
+        axs[i,j].set_ylabel('y')
+        axs[i,j].set_zlabel('value')
+    
+    plt.tight_layout()
     plt.savefig(savefile)
     plt.clf()
     # will remove a specific figure instance from the pylab state machine
